db241e9b065990b44bc5e69869d7e0e1825c9b01
kotaro.kawashiki <kotaro.kawashiki@rakuten.com>
2018-06-15 03:33:22 +0000

    ranking

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,8 @@ Route::group(['middleware' => ['auth']], function () {
     Route::delete('have', 'ItemUserController@dont_have')->name('item_user.dont_have');
     
     Route::resource('users', 'UsersController', ['only' => ['show']]);
+//ranking function
+Route::get('ranking/want', 'RankingController@want')->name('ranking.want');
+Route::get('ranking/have', 'RankingController@have')->name('ranking.have');
+
 });

acf593c382ce1eb73243c1a8dbf2d45637ac0584
kotaro.kawashiki <kotaro.kawashiki@rakuten.com>
2018-06-15 01:26:31 +0000

    have function

--- app/User.php
+++ app/User.php
@@ -32,6 +32,8 @@ class User extends Authenticatable
         return $this->belongsToMany(Item::class)->withPivot('type')->withTimestamps();
     }
 
+    //want function
+
     public function want_items()
     {
         return $this->items()->where('type', 'want');
@@ -73,6 +75,46 @@ class User extends Authenticatable
             return $item_id_exists;
         } else {
             $item_code_exists = $this->want_items()->where('code', $itemIdOrCode)->exists();
+            return $item_code_exists;
+        }
+    }
+    
+    //have function
+    
+    public function have_items(){
+        return $this->items()->where('type', 'have');
+    }
+    
+    public function have($itemID){
+        $exist = $this->is_having($itemID);
+        
+        if($exist){
+            return false;
+        }
+        else{
+            $this->items()->attach($itemID, ['type' => 'have']);
+            return true;
+        }
+    }
+    
+    public function dont_have($itemID){
+        $exist = $this->is_having($itemID);
+        
+        if($exist){
+             \DB::delete("DELETE FROM item_user WHERE user_id = ? AND item_id = ? AND type = 'have'", [\Auth::user()->id, $itemID]);
+        }
+        else{
+            return false;
+        }
+    }
+    
+    public function is_having($itemIDOrCode){
+        
+        if (is_numeric($itemIDOrCode)) {
+            $item_id_exists = $this->have_items()->where('item_id', $itemIDOrCode)->exists();
+            return $item_id_exists;
+        } else {
+            $item_code_exists = $this->have_items()->where('code', $itemIDOrCode)->exists();
             return $item_code_exists;
         }
     }
